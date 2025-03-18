ad77665b21e967e677bcdf740f582c10228c31d3
Leandro Martinez <lmartine@unicamp.br>
2025-03-18 14:52:25 -0300

    add test file and adjust pbc call

--- src/pbc.f90
+++ src/pbc.f90
@@ -4,25 +4,29 @@
 
 module pbc
 
-   double precision, public :: pbc_box(6), pbc_length(3)
+   implicit none
+
+   double precision, public :: pbc_box(6), pbc_sides(3)
    logical, public :: using_pbc = .false.
 
    public delta_vector, idx_box
 
 contains
 
-   elemental double precision function delta_vector(v1,v2,pbc_length)
-      implicit none
-      double precision, intent(in) :: v1, v2, pbc_length
-      delta_vector = v1 - v2
+   elemental double precision function delta_vector(v2,v1,using_pbc,pbc_sides)
+      logical, intent(in) :: using_pbc
+      double precision, intent(in) :: v1, v2, pbc_sides
+      delta_vector = v2 - v1
       if (using_pbc) then
-         delta_vector = delta_vector - pbc_length * nint(delta_vector/pbc_length)
+         delta_vector = mod(delta_vector, pbc_sides)
+         if (delta_vector >= pbc_sides / 2) then
+            delta_vector = pbc_sides - delta_vector
+         end if
       end if
    end function delta_vector
 
    integer function idx_box(ibox, nbox)
-      implicit none
-      integer ibox, nbox
+      integer :: ibox, nbox
       if (using_pbc) then
          idx_box = modulo(ibox - 1 + nbox, nbox) + 1
       else
