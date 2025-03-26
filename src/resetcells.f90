fc7924729a9a896c5a0f8f094ffd2ec2e78bba2a
Leandro Martinez <lmartine@unicamp.br>
2025-03-26 16:21:50 -0300

    explicitly reset cells

--- src/resetcells.f90
+++ src/resetcells.f90
@@ -8,22 +8,36 @@
 !
 
 subroutine resetcells()
-
-   use cell_indexing, only: icell_to_cell 
-   use compute_data, only: latomfirst, latomfix, lcellfirst, lcellnext, hasfree, ncells
+   use input, only: fix, ntype_with_fixed
+   use cell_indexing, only: icell_to_cell, index_cell, setcell
+   use compute_data, only: latomfirst, lcellfirst, lcellnext, hasfree, ncells, &
+                           ntype, natoms, ntotat, nfixedat, xcart, latomnext
    implicit none
-   integer :: cell(3), icell
+   integer :: cell(3), icell, iftype, icart, ifatom
 
    ! Reset data for cells that contain fixed atom
-
-   icell = lcellfirst
-   do while( icell > 0 )
-      call icell_to_cell(icell,ncells,cell)
-      latomfirst(cell(1),cell(2),cell(3)) = latomfix(cell(1),cell(2),cell(3))
-      hasfree(cell(1),cell(2),cell(3)) = .false.
-      icell = lcellnext(icell)
-   end do
+   lcellnext(:) = 0
    lcellfirst = 0
+   latomfirst(:,:,:) = 0
+   latomnext(:) = 0
+   hasfree(:,:,:) = .false.
+   if(fix) then
+      icart = ntotat - nfixedat
+      do iftype = ntype + 1, ntype_with_fixed
+         do ifatom = 1, natoms(iftype)
+            icart = icart + 1
+            call setcell(xcart(icart,:),cell)
+            latomnext(icart) = latomfirst(cell(1),cell(2),cell(3))
+            latomfirst(cell(1),cell(2),cell(3)) = icart
+            if (.not. hasfree(cell(1),cell(2),cell(3)) ) then
+               hasfree(cell(1),cell(2),cell(3)) = .true.
+               icell = index_cell(cell,ncells)
+               lcellnext(icell) = lcellfirst
+               lcellfirst = icell
+            end if
+         end do
+      end do
+   end if
 
 end subroutine resetcells
 
