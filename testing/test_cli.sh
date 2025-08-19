e915db2ffa05fe6ae94d6734a6a440eee247d557
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-08-19 17:06:03 -0400

    adds notes to shell script
    NOTES
    the comments are meant to explain what it is being done from a high
    level, though developers and users alike are encouraged to read
    the man pages of the tools that we have used to understand the test
    in more depth

--- testing/test_cli.sh
+++ testing/test_cli.sh
@@ -20,6 +20,7 @@
 
 verbose=0
 
+# checks number of command-line arguments given
 if [ "$#" -ne 1 ] ; then
 	echo "Error: expects the path to packmol as the first command-line argument"
 	echo "Example: ./test_cli.sh /usr/bin/packmol"
@@ -27,6 +28,7 @@ if [ "$#" -ne 1 ] ; then
 fi
 
 packmol="$1"
+# checks that we can execute packmol
 if ! [ -f "$packmol" ] ; then
 	echo "Error: packmol does not exist"
 	exit 1
@@ -37,11 +39,13 @@ if ! [ -x "$packmol" ] ; then
 	exit 1
 fi
 
+# checks that the input_files directo exists
 if ! [ -d input_files ] ; then
 	echo "Error: the 'input_files' directory does not exist"
 	exit 1
 fi
 
+# checks that all the commands this script depends exists for the sake of completeness
 ls=$(which ls)
 if ! [ -x "$ls" ] ; then
 	echo "Error: ls command not found"
@@ -97,6 +101,7 @@ if ! [ -x "$uname" ] ; then
 fi
 
 os=$("$uname")
+# OSX does not ship with GNU gawk by default so we need to use what they provide
 if [ "Darwin" == "$os" ] ; then
 	gawk=$(which awk)
 else
@@ -108,8 +113,14 @@ if ! [ -x "$gawk" ] ; then
 	exit 1
 fi
 
+# for each input file in the input_files directory run packmol and check that we get the
+# same results (same contents in the output file) regardless of the invokation method
+# used to execute packmol
 "$ls" input_files/*.inp | while read input_file
 do
+	# here we use pattern matching to exclude input_files that we know would fail
+	# because they are meant to fail; since that would mess our test we filter
+	# those out with grep
 	sw=$(echo "$input_file" | "$grep" -v "error" | "$grep" -v "fail" | "$wc" -c)
 	if [ "$sw" -eq 0 ] ; then
 		if [ "$verbose" -eq 1 ] ; then
@@ -118,6 +129,9 @@ do
 		continue
 	fi
 
+	# we need to patch some of the data in the original input files this is why we
+	# create .txt and .tmp versions; we use regex to replace the .inp with either
+	# .txt or .tmp extensions
 	input_txt=$(\
 		echo "$input_file" |\
 		"$sed" 's/\.inp/.txt/g'
@@ -128,6 +142,10 @@ do
 		"$sed" 's/\.inp/.tmp/g'
 	)
 
+	# We fix the `seed` value on those input_files that set it to -1; otherwise
+	# packmol could yield different results and we don't want that of course, we
+	# want reproducible results for the same input. That is why we provide a fix
+	# value for the seed.
 	"$cp" "$input_file" "$input_txt"
 	"$sed" -e 's/seed -1/seed 1024/g' -i'.tmp' "$input_txt"
 	"$cp" "$input_txt" "$input_tmp"
@@ -135,17 +153,20 @@ do
 	"$rm" "$input_txt.tmp"
 	"$rm" "$input_tmp.tmp"
 
+	# here we extract the name of the output file from the input-file
 	output_pdb=$(\
 		"$cat" "$input_txt" | \
 		"$grep" "output" | \
 		"$gawk" '{print $2}'
 	)
 
+	# uses substitution to produce an output file with a .txt file extension
 	output_txt=$(\
 		echo "$output_pdb" | \
 		"$sed" 's/\.pdb/.txt/g'
 	)
 
+	# uses substitution to produce an output file with a .tmp file extension
 	output_tmp=$(\
 		echo "$output_pdb" | \
 		"$sed" 's/\.pdb/.tmp/g'
@@ -153,6 +174,7 @@ do
 
 	echo -n "Running test $input_txt ... "
 
+	# runs packmol in the usual way with input redirection
 	if ! "$packmol" < "$input_txt" 1>/dev/null 2>/dev/null ; then
 		echo "FAIL"
 		"$rm" -f "$input_tmp"
@@ -163,6 +185,11 @@ do
 		exit 1
 	fi
 
+	# Provides the input file via command-line to packmol and executes it;
+	# we redirect the standard output and error to the null-device to keep
+	# our test output clean. In this case the output file is determined from
+	# the input-file `input_tmp`; note that the output file will have a .tmp
+	# file extension because of the preprocessing that we have done.
 	if ! "$packmol" -i "$input_tmp" 1>/dev/null 2>/dev/null ; then
 		echo "FAIL"
 		"$rm" -f "$input_tmp"
@@ -173,6 +200,7 @@ do
 		exit 1
 	fi
 
+	# provides both the input and output files to packmol and executes it
 	if ! "$packmol" -i "$input_txt" -o "$output_txt" 1>/dev/null 2>/dev/null ; then
 		echo "FAIL"
 		"$rm" -f "$input_tmp"
@@ -183,6 +211,9 @@ do
 		exit 1
 	fi
 
+	# here we check that the output files are identical via GNU diffutil;
+	# to do that we concatenate the diff contents and determine the number of
+	# bytes (we should get zero bytes when they are the same)
 	res1=$("$diff" --normal "$output_pdb" "$output_txt" | "$wc" -c)
 	res2=$("$diff" --normal "$output_pdb" "$output_tmp" | "$wc" -c)
 	if [ "$res1" -eq 0 ] && [ "$res2" -eq 0 ] ; then
@@ -197,6 +228,7 @@ do
 		exit 1
 	fi
 
+	# we clean up the testing directory from our temporary files
 	"$rm" -f "$input_tmp"
 	"$rm" -f "$input_txt"
 	"$rm" -f "$output_txt"
@@ -204,4 +236,5 @@ do
 	"$rm" -f "$output_tmp"
 done
 
+# if we get here packmol passed all the tests successfully
 exit 0
