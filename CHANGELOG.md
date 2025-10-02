fc99f261b11295956844e7bd6b64dbf82ccd7763
Leandro Martinez <lmartine@unicamp.br>
2025-10-02 08:43:41 -0300

    change update_version.sh release script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,15 +10,19 @@ packmol changelog
 [badge-fix]: https://img.shields.io/badge/Fix-purple.svg
 [badge-info]: https://img.shields.io/badge/Info-gray.svg
 
+Version 21.1.2-DEV
+--------------
+- ![INFO][badge-info] The `release.sh` script was renamed to `update_version.sh` and just updates version numbers in the files, without tagging a release.
+
 Version 21.1.1
--------------
+--------------
 - ![INFO][badge-info] Update minimum CMake compatibility to 3.5.0.
 - ![INFO][badge-info] Add a remark to what is written as `CRYST1` information when no-PBCs are used. Suggest using PBCs Fixes [103](https://github.com/m3g/packmol/issues/103). Update the documentation accordingly.
 
 Version 21.1.0
--------------
+--------------
 - ![FEATURE][badge-feature] Support CLI input and output file specifications (i. e. `-i input.inp -o output.pdb`) ([PR](https://github.com/m3g/packmol/pull/101)).
 
 Version 21.0.4
--------------
+--------------
 - For older versions, see the [releases page](https://github.com/m3g/packmol/releases).
--- update_version.sh
+++ update_version.sh
@@ -1,11 +1,8 @@
 ####################################################################################################
-
 #
-# How to release the software:
+# How to update the version number:
 #
-# run ./release. 21.0.1 (for example)
-#
-# go to the github site and release the version corresponding to the new tag.
+# run: ./update_version 21.0.1 (for example)
 #
 
 # Software name:
@@ -15,22 +12,12 @@ package=packmol
 # Release version, read from command line:
 version="$1"
 
-# GIT URL:
-
-giturl=https://github.com/m3g/packmol
-
 # Name of files containing version number
 
 versionfile=./src/title.f90
 fpmfile=./fpm.toml
 
 ####################################################################################################
-
-#git log --pretty=oneline 16.323...16.330 | awk '{$1=""; print "-"$0}'
-
-year=`date +%y`
-day=`date +%j`
-#version="${year:0:1}${year:1:1}.$day"
 if [[ $version < " " ]]; then
   version=`grep version fpm.toml`
   echo "ERROR: Please provide version number, with: ./release.sh 20.1.1"
@@ -44,13 +31,8 @@ cat $versionfile | sed -e "s/Version.*/Version\ $version \')\")/" > tmpfile.txt
 cat $fpmfile | sed -e "s/version.*/version = \"$version\"/" > tmpfile.txt
 \mv -f tmpfile.txt $fpmfile
 
-git add -A .
-git commit -m "Changed version file to $version"
-git tag -a "v$version" -m "Release $version"
-git push origin master tag "v$version"
-
 echo "----------------------"
-echo "CHANGE LOG:"
+echo "Please update the CHANGELOG.md file. Commits:"
 echo "----------------------"
 range=`git tag | sort -V | tail -n 2 | xargs | sed 's! !...!'`
 git log --pretty=oneline $range | awk '{$1=""; print "-"$0}'
