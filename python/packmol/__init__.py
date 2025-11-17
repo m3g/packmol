fbb9abec9394a8d681e674dcfd614a0385eac6f4
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-11-17 11:08:28 +0100

    hatch based build system

---
+++ python/packmol/__init__.py
@@ -0,0 +1,7 @@
+"""Packmol - A package for creating initial configurations for molecular dynamics simulations."""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("packmol")
+
+__all__ = ["__version__"]
