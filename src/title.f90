a4baf7d1568c3d9fd348d9a354b30eccde5ac645
PythonFZ <fabian.zills@web.de>
2025-11-26 22:00:29 +0100

    bump version

--- src/title.f90
+++ src/title.f90
@@ -13,7 +13,7 @@ subroutine title()
     write(*,"(' PACKMOL - Packing optimization for the automated generation of', /&
              &' starting configurations for molecular dynamics simulations.', /&
              &' ',/&
-             &t61,' Version 21.1.3 ')")
+             &t61,' Version 21.1.4 ')")
     write(*,hash3_line)
 
 end subroutine title
