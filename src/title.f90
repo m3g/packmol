7f8e0c6f945eeffe123279e09a40b1cdd04fc0ec
Leandro Martinez <lmartine@unicamp.br>
2026-01-27 08:24:18 -0300

    set version to 21.2.1

--- src/title.f90
+++ src/title.f90
@@ -13,7 +13,7 @@ subroutine title()
     write(*,"(' PACKMOL - Packing optimization for the automated generation of', /&
              &' starting configurations for molecular dynamics simulations.', /&
              &' ',/&
-             &t61,' Version 21.2.0 ')")
+             &t61,' Version 21.2.1 ')")
     write(*,hash3_line)
 
 end subroutine title
