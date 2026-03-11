d01d2c0f1cb80b407fad2ea28e3699f59858dc16
Michele Bonus <77387356+MicheleBonus@users.noreply.github.com>
2026-03-11 10:29:33 +0100

    Optimize cell neighbor lookup in pairwise loops

--- src/computef.f90
+++ src/computef.f90
@@ -17,6 +17,10 @@ subroutine computef(n,x,f)
    integer :: n, i, j, k, icell
    integer :: ilugan, ilubar, icart, itype, imol, iatom, idatom
    integer :: cell(3)
+   integer :: ip1, jp1, kp1, jm1, km1
+   integer :: neigh000, neigh100, neigh010, neigh001
+   integer :: neigh1m10, neigh10m1, neigh01m1, neigh011, neigh110, neigh101
+   integer :: neigh1m1m1, neigh1m11, neigh11m1, neigh111
 
    double precision :: v1(3), v2(3), v3(3)
    double precision :: x(n)
@@ -120,28 +124,49 @@ subroutine computef(n,x,f)
       j = cell(2)
       k = cell(3)
 
-      icart = latomfirst(i,j,k)
+      ip1 = cell_ind(i + 1, ncells(1))
+      jp1 = cell_ind(j + 1, ncells(2))
+      kp1 = cell_ind(k + 1, ncells(3))
+      jm1 = cell_ind(j - 1, ncells(2))
+      km1 = cell_ind(k - 1, ncells(3))
+
+      neigh000 = latomfirst(i,j,k)
+      neigh100 = latomfirst(ip1,j,k)
+      neigh010 = latomfirst(i,jp1,k)
+      neigh001 = latomfirst(i,j,kp1)
+      neigh1m10 = latomfirst(ip1,jm1,k)
+      neigh10m1 = latomfirst(ip1,j,km1)
+      neigh01m1 = latomfirst(i,jp1,km1)
+      neigh011 = latomfirst(i,jp1,kp1)
+      neigh110 = latomfirst(ip1,jp1,k)
+      neigh101 = latomfirst(ip1,j,kp1)
+      neigh1m1m1 = latomfirst(ip1,jm1,km1)
+      neigh1m11 = latomfirst(ip1,jm1,kp1)
+      neigh11m1 = latomfirst(ip1,jp1,km1)
+      neigh111 = latomfirst(ip1,jp1,kp1)
+
+      icart = neigh000
       do while( icart > 0 )
 
          if(comptype(ibtype(icart))) then
             ! Interactions inside cell
             f = f + fparc(icart,latomnext(icart))
             ! Interactions of cells that share faces (6 faces - 3 forward)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),j,k)) ! 4 - (1, 0, 0)
-            f = f + fparc(icart,latomfirst(i,cell_ind(j+1, ncells(2)),k)) ! 5 - (0, 1, 0)
-            f = f + fparc(icart,latomfirst(i,j,cell_ind(k+1, ncells(3)))) ! 6 - (0, 0, 1)
+            f = f + fparc(icart,neigh100) ! 4 - (1, 0, 0)
+            f = f + fparc(icart,neigh010) ! 5 - (0, 1, 0)
+            f = f + fparc(icart,neigh001) ! 6 - (0, 0, 1)
             ! Interactions of cells that share axes (12 edges - 6 forward)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j-1, ncells(2)),k)) ! 4 - (1, -1, 0)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),j,cell_ind(k-1, ncells(3)))) ! 5 - (1, 0, -1)
-            f = f + fparc(icart,latomfirst(i,cell_ind(j+1, ncells(2)),cell_ind(k-1, ncells(3)))) ! 6 - (0, 1, -1)
-            f = f + fparc(icart,latomfirst(i,cell_ind(j+1, ncells(2)),cell_ind(k+1, ncells(3)))) ! 9 - (0, 1, 1)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j+1, ncells(2)),k)) ! 10 - (1, 1, 0)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),j,cell_ind(k+1, ncells(3)))) ! 11 - (1, 0, 1)
+            f = f + fparc(icart,neigh1m10) ! 4 - (1, -1, 0)
+            f = f + fparc(icart,neigh10m1) ! 5 - (1, 0, -1)
+            f = f + fparc(icart,neigh01m1) ! 6 - (0, 1, -1)
+            f = f + fparc(icart,neigh011) ! 9 - (0, 1, 1)
+            f = f + fparc(icart,neigh110) ! 10 - (1, 1, 0)
+            f = f + fparc(icart,neigh101) ! 11 - (1, 0, 1)
             ! Interactions of cells that share vertices (8 vertices, 8 forward)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j-1, ncells(2)),cell_ind(k-1, ncells(3)))) ! 5 - (1, -1, -1)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j-1, ncells(2)),cell_ind(k+1, ncells(3)))) ! 6 - (1, -1, 1)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j+1, ncells(2)),cell_ind(k-1, ncells(3)))) ! 7 - (1, 1, -1)
-            f = f + fparc(icart,latomfirst(cell_ind(i+1, ncells(1)),cell_ind(j+1, ncells(2)),cell_ind(k+1, ncells(3)))) ! 8 - (1, 1, 1)
+            f = f + fparc(icart,neigh1m1m1) ! 5 - (1, -1, -1)
+            f = f + fparc(icart,neigh1m11) ! 6 - (1, -1, 1)
+            f = f + fparc(icart,neigh11m1) ! 7 - (1, 1, -1)
+            f = f + fparc(icart,neigh111) ! 8 - (1, 1, 1)
          end if
 
          icart = latomnext(icart)
